a3108ca2972879e20678e14fdc5c8897573d92c3
[DarlinHolguin] <[darlinholguin123@gmail.com]>
2025-11-18 23:54:39 -0500

    14 Passed

--- src/library-items.js
+++ src/library-items.js
@@ -1,14 +1,49 @@
 /* eslint-disable max-classes-per-file */
 
 class LibraryItem {
+  isCheckedOut = false
+constructor(title, year) {
+this.title = title
+this.year = year
+}
+
+checkOut() {
+  this.isCheckedOut = true
+  return `${this.title} has been checked out`
+}
+
+returnItem() {
+  this.isCheckedOut = false
+  return `${this.title} has been returned`
+}
+
+getDescription(){
+return `${this.title} (${this.year})`
 
 }
 
-class Book {
 
 }
 
-class DVD {
+class Book extends LibraryItem {
+  constructor(author, pages) {
+    super(title, year)
+    this.author = author
+    this.page = pages
+  }
+
+  getDescription(){
+    return `${super.getDescription()} by ${this.author}, ${this.page} pages`
+  }
+
+  
+
+}
+
+class DVD extends LibraryItem{
+  constructor() {
+    
+  }
 
 }
 
